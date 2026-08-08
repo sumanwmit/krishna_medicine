da80ebeb449c225349408205d992ad7c2f254b93
sumanwmit <suman.wmit@gmail.com>
2026-08-08 16:36:24 +0530

    Update useWmitTracker.ts

--- src/hooks/useWmitTracker.ts
+++ src/hooks/useWmitTracker.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function useWmitTracker() {
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
     
